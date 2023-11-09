3f02e1411d91c9f9ff461b943c197f0d0b652369
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-09 09:25:25 +0100

    Données factives 19.4 fait
    création des données pour les catégories et modification des contacts -> ils ont maintenant 90% de chance d'avoir une catégorie

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -7,19 +7,12 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class CategoryFixtures extends Fixture implements DependentFixtureInterface
+class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $file = file_get_contents(__DIR__.'/data/Category.json');
         json_decode($file, true);
         CategoryFactory::createMany(8);
-    }
-
-    public function getDependencies(): array
-    {
-        return [
-            ContactFixtures::class,
-        ];
     }
 }
--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\ContactFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ContactFixtures extends Fixture
+class ContactFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        ContactFactory::createMany(150);
-        ContactFactory::faker()->boolean(90);
+        ContactFactory::createMany(150, function () {
+            if (ContactFactory::faker()->boolean(90)) {
+                return [
+                    'category' => CategoryFactory::random(),
+                ];
+            }
+            return [];
+        });
+    }
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
