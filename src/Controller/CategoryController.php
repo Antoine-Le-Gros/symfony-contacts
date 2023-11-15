42d108cd1adddda57b92b2f65094bec76758e35b
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-15 09:53:09 +0100

    ajout du show dans le controller category et la template show de la categorie

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
-use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,14 +17,14 @@ class CategoryController extends AbstractController
 
         return $this->render('category/index.html.twig', ['categories' => $categories]);
     }
+
     #[Route('/category/{id}')]
     public function show(?Category $category): Response
     {
-        $contactRepository = new ContactRepository();
         if (empty($category)) {
             throw $this->createNotFoundException("la catégorie n'existe pas ");
         }
-        $contacts = $contactRepository->search($search);
-        return $this->render('contact/_contacts.html.twig', ['contacts' => $contacts])
+
+        return $this->render('category/show.html.twig', ['category' => $category]);
     }
 }
