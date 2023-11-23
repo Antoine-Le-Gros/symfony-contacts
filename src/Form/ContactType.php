2c57618d7cc672df333922edd8d97f09a546507e
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-11-23 10:10:21 +0100

    commit fin de séance
    21.2.6 problème pas de formulaires affichés

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +19,14 @@ class ContactType extends AbstractType
             ->add('lastname')
             ->add('email')
             ->add('phone')
-            ->add('category')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'query_builder' => function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('c')
+                        ->orderBy('c.name', 'ASC');
+                },
+            ])
         ;
     }
 
