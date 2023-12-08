0125ac81bb2f4718bb9b9f87a872d7935a289e8d
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-08 22:38:54 +0100

    Modification de la méthode getDefaults de la factory User

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -42,10 +42,10 @@ final class UserFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            'email' => self::faker()->text(180),
-            'firstname' => self::faker()->text(100),
-            'lastname' => self::faker()->text(150),
-            'password' => self::faker()->text(),
+            'email' => self::faker()->unique()->numerify('user-####@example.com'),
+            'firstname' => self::faker()->firstName(),
+            'lastname' => self::faker()->lastName(),
+            'password' => 'test',
             'roles' => [],
         ];
     }
