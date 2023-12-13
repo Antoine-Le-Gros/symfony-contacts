b1b57c31c3773352576f836acfe051a717e5eb2b
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-13 10:09:40 +0100

    Modification des tests CreateCest.php

--- tests/Controller/Contact/CreateCest.php
+++ tests/Controller/Contact/CreateCest.php
@@ -2,15 +2,34 @@
 
 namespace App\Tests\Controller\Contact;
 
+use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
 
 class CreateCest
 {
-    public function form(ControllerTester $I): void
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
         $I->amOnPage('/contact/create');
-
+        $I->seeCurrentRouteIs('app_login');
+    }
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
+    {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_USER'],
+        ])->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/contact/create');
+        $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeInTitle("Création d'un nouveau contact");
-        $I->see("Création d'un nouveau contact", 'h1');
     }
 }
