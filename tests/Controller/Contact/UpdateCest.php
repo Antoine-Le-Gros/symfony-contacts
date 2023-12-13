5aa815382b7b7262da436f8c1a01af79368df554
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-13 10:00:17 +0100

    Modification des tests pour la modification

--- tests/Controller/Contact/UpdateCest.php
+++ tests/Controller/Contact/UpdateCest.php
@@ -3,20 +3,42 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
+use Zenstruck\Assert;
 
 class UpdateCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactDataBeforeUpdating(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_ADMIN'],
+        ])->object();
+        $I->amLoggedInAs($user);
+    }
+
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',
         ]);
-
         $I->amOnPage('/contact/1/update');
+        $I->seeCurrentRouteIs('app_login');
+    }
 
-        $I->seeInTitle('Édition de Simpson, Homer');
-        $I->see('Édition de Simpson, Homer', 'h1');
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
+    {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_USER'],
+        ])->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/contact/1/update');
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
 }
