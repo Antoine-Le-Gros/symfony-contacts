d85d1029b9b6d1f464104faeb2d05d04d6a25d7f
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-13 10:01:20 +0100

    Modification des tests pour la suppressions des contacts

--- tests/Controller/Contact/DeleteCest.php
+++ tests/Controller/Contact/DeleteCest.php
@@ -3,20 +3,41 @@
 namespace App\Tests\Controller\Contact;
 
 use App\Factory\ContactFactory;
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
+use Codeception\Util\HttpCode;
 
 class DeleteCest
 {
-    public function form(ControllerTester $I): void
+    public function formShowsContactDataBeforeUpdating(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_ADMIN'],
+        ])->object();
+        $I->amLoggedInAs($user);
+    }
+
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
         ContactFactory::createOne([
             'firstname' => 'Homer',
             'lastname' => 'Simpson',
         ]);
-
         $I->amOnPage('/contact/1/delete');
+        $I->seeCurrentRouteIs('app_login');
+    }
 
-        $I->seeInTitle('Suppression de Simpson, Homer');
-        $I->see('Suppression de Simpson, Homer', 'h1');
+    public function accessIsRestrictedToAdminUsers(ControllerTester $I): void
+    {
+        ContactFactory::createOne([
+            'firstname' => 'Homer',
+            'lastname' => 'Simpson',
+        ]);
+        $user = UserFactory::createOne([
+            'roles' => ['ROLE_USER'],
+        ])->object();
+        $I->amLoggedInAs($user);
+        $I->amOnPage('/contact/1/delete');
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
 }
