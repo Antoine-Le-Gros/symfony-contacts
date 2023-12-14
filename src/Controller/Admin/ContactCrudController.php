ebeb41113deb7032226c94a2a109d798cff58a81
Antoine LE GROS <antoine.le-gros@etudiant.univ-reims.fr>
2023-12-14 10:04:43 +0100

    Configuration du « CRUD » de « Contact »
    Modification de la page des contacts dans le /admin
    Ajout de l'affichage des catégories

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,9 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ContactCrudController extends AbstractCrudController
@@ -15,14 +18,25 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('lastname', 'Nom'),
+            EmailField::new('email', 'Adresse Mail'),
+            TelephoneField::new('phone', 'Téléphone'),
+            AssociationField::new('category', 'Catégorie')
+                ->formatValue(function ($value, Contact $contact) {
+                    return $contact->getCategory()?->getName();
+                })
+                ->setFormTypeOptions([
+                    'choice_label' => 'name',
+                    'query_builder' => function (EntityRepository $entityRepository) {
+                        return $entityRepository->createQueryBuilder('c')
+                            ->orderBy('c.name', 'ASC');
+                    },
+            ]),
         ];
     }
-    */
 }
